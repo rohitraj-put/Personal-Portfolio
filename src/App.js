e6926d16840fd9f613a8dda54f3ce5fc3797f3c2
rohit rajput <rohitraj05072000@gmail.com>
2024-02-21 11:14:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,30 @@ import About from './portfolio/About';
 import Project from './portfolio/Project';
 import Contact from './portfolio/Contact';
 import Footer from './portfolio/Footer';
+import tech from './Image/tech.png'
+import Review from './portfolio/Review';
+
 
 
 const App = () => {
   return (<>
+ 
     <Header ownerName="Rohit Rajput" fileName="projects"
       contant="A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product"
     />
-    <About btn="contact" />
-    <Project fileName="projects" />
-    <Contact btn="contact" />
+    <About btn="contact" fileName="projects" />
+    <div className="pro">
+      <Project
+
+      />
+    </div>
+    <Review/>
+    <Contact btn="contact" imgsrc={tech} />
+    
     <Footer ownerName="Rohit Rajput"
       contant="A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product"
     />
+
   </>
   )
 }
---
+++ src/portfolio/Project.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import age from '../Image/age.png'
+import tic from '../Image/tic.png'
+import netflix from '../Image/netflix.png'
+import jupiter from '../Image/jupiter.png'
+import portfolio from '../Image/portfolio.png'
+
+const Project = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000
+    });
+  }, []);
+  let live = "LIVE CODE"
+
+  const mainpros = [
+    {
+      srcname: portfolio,
+      proroot: '#'
+    },
+    {
+      srcname: jupiter,
+      proroot: 'https://rohitraj-put.github.io/Jupiter-animation/'
+    },
+    {
+      srcname: age,
+      proroot: 'https://rohitraj-put.github.io/Age-Calculator/'
+    },
+    {
+      srcname: netflix,
+      proroot: 'https://rohitraj-put.github.io/ToDo-List/'
+    },
+    {
+      srcname: tic,
+      proroot: 'https://rohitraj-put.github.io/Tic-tac-toe/'
+    }
+    
+
+  ]
+
+  let allpro = mainpros.map(mainpro => <div className='box-pro' key={mainpro.srcname}>
+  <img src={mainpro.srcname} alt='project-img' data-aos="flip-left" rel="noopener"/>
+  <button className='live-code'><a href={mainpro.proroot} target='_blank'>{live}</a></button>
+  </div>)
+
+  return (
+    <>
+      {allpro}
+      
+    </>
+  )
+}
+
+export default Project
